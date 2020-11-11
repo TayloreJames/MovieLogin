807a410b2f41eadd8feb50c344a59455a54530d1
TayloreJames <67704935+TayloreJames@users.noreply.github.com>
2020-11-11 00:42:47 -0500

    first commit

---
+++ Data/Context/MovieDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Lab21_24.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab21_24.Data.Context
+{
+    public class MovieDbContext:DbContext
+    {
+        public MovieDbContext(DbContextOptions<MovieDbContext> options):base(options)
+        {
+        }
+
+        public DbSet<MovieModel> Movies { get; set; }
+    }
+}
